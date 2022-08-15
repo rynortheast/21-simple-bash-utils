78aabc5d7f28e814cd8736fe4682189cb26a3d67
chamomil <chamomil@student.21-school.ru>
2022-08-15 17:02:00 +0300

    Добавлена базовая структура подпроекта cat

---
+++ cat/s21_cat.c
@@ -0,0 +1,6 @@
+#include "s21_cat.h"
+
+int main(int argc, char **argv) {
+    s21_cat(argc, argv);
+    return EXIT_SUCCESS;
+}
---
+++ cat/s21_cat.h
@@ -0,0 +1,16 @@
+#ifndef SRC_CAT_S21_CAT_H_
+#define SRC_CAT_S21_CAT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int b;
+    int e;
+    int n;
+    int s;
+    int t;
+} options;
+
+#endif  // SRC_CAT_S21_CAT_H_
